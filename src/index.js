b3f565e67fca847ece2015fb19e713d809200de9
unknown <gestion.informatica@esiapi.edu.mx>
2024-02-23 11:50:46 -0600

    [ADD] EXPRESS JSON 3

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const cors =  require("cors")
+const mysql = require("./DBConnection/index")
+const exc = require("./DBConnection/excecutions")
+const port = process.env.PORT || 4000;
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.get("/", (req, res) =>{
+    res.send("Wellcome to Api-Esi-List");
+})
+app.get("/lic", (req,res)=>{
+    exc(mysql().db, req, res).licenciatura()
+})
+app.get("/pos", (req,res)=>{
+    exc(mysql().db, req, res).posgrado()
+})
+app.listen(port, ()=> console.log("PORT YOU'RE USING IS:", port));
