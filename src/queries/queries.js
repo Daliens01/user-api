cdeb46f6c02083759804e8b6c457b7895fae193c
unknown <gestion.informatica@esiapi.edu.mx>
2024-02-28 10:54:17 -0600

    [CHANGE] const date

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -1,8 +1,8 @@
 const queries = ()=>{
-    const date = `2024-02-22`
+    const date = "2024-02-28"
 
     const licenciatura = `SELECT u.username AS MATRICULA, CONCAT(u.firstname," ", u.lastname) AS ALUMNO, c.fullname AS CURSOS
-    ,IF(DATEDIFF(${date},FROM_UNIXTIME(timeaccess))>=9, CONCAT(DATEDIFF(${date},FROM_UNIXTIME(timeaccess))," Días sin acceder"),"NUNCA") AS ACCESO
+    ,IF(DATEDIFF("${date}",FROM_UNIXTIME(timeaccess))>=9, CONCAT(DATEDIFF("${date}",FROM_UNIXTIME(timeaccess))," Días sin acceder"),"NUNCA") AS "ÚLTIMO ACCESO"
     FROM
     mdl_user u
     LEFT JOIN mdl_role_assignments r ON r.userid = u.id
@@ -14,10 +14,10 @@ const queries = ()=>{
     r.roleid = 5
     AND c.id IN (965,964,963,962,961,960,959,958,957,956,955,954,953,952,951,950,949,948,947,946,945)
     AND u.id NOT IN (618)
-    AND (DATEDIFF(${date},FROM_UNIXTIME(timeaccess)) >=9 OR DATEDIFF(${date},FROM_UNIXTIME(timeaccess)) IS NULL)
+    AND (DATEDIFF("${date}",FROM_UNIXTIME(timeaccess)) >=9 OR DATEDIFF("${date}",FROM_UNIXTIME(timeaccess)) IS NULL)
     ORDER BY ALUMNO, CURSOS DESC;`
     const posgrado = `SELECT u.username AS MATRICULA, CONCAT(u.firstname," ", u.lastname) AS ALUMNO, c.fullname AS CURSOS
-    ,IF(DATEDIFF(${date},FROM_UNIXTIME(timeaccess))>=9, CONCAT(DATEDIFF(${date},FROM_UNIXTIME(timeaccess))," Días sin acceder"),"NUNCA") AS ACCESO
+    ,IF(DATEDIFF("${date}",FROM_UNIXTIME(timeaccess))>=9, CONCAT(DATEDIFF("${date}",FROM_UNIXTIME(timeaccess))," Días sin acceder"),"NUNCA") AS ACCESO
     FROM
     mdl_user u
     LEFT JOIN mdl_role_assignments r ON r.userid = u.id
@@ -27,9 +27,9 @@ const queries = ()=>{
     LEFT JOIN mdl_user_lastaccess lt ON  lt.courseid = c.id AND lt.userid = u.id
     WHERE
     r.roleid = 5
-    AND c.id IN (666,667,668,669,658,659,660,661,662,663,664,665,670)
+    AND c.id IN (925,926,928,917,918,919,920,921,922,923,924,929)
     AND u.id NOT IN (618)
-    AND (DATEDIFF(${date},FROM_UNIXTIME(timeaccess)) >=9 OR DATEDIFF(${date},FROM_UNIXTIME(timeaccess)) IS NULL)
+    AND (DATEDIFF("${date}",FROM_UNIXTIME(timeaccess)) >=9 OR DATEDIFF("${date}",FROM_UNIXTIME(timeaccess)) IS NULL)
     ORDER BY CURSOS ASC, ALUMNO;`
 
     return {licenciatura,posgrado}
