16bac78ceadf9f804492aba18a5bd3efb62cfc94
unknown <gestion.informatica@esiapi.edu.mx>
2025-01-16 10:12:54 -0600

    adding b4 24-25

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -1,7 +1,7 @@
 const queries = ()=>{
     const fecha = new Date()
     const TodaysDate = `${fecha.getFullYear()}-${fecha.getMonth()+1 > 9?fecha.getMonth()+1:`0${fecha.getMonth()+1}`}-${fecha.getDate() > 9? fecha.getDate(): `0${fecha.getDate()}`}`;
-    //bloque 3 24-24
+    //bloque 3 24-25
     const licenciatura = `SELECT u.username AS MATRICULA, CONCAT(u.firstname," ", u.lastname) AS ALUMNO, c.fullname AS CURSOS
     ,CASE
 	WHEN u.id =618 THEN "NO APLICA"
@@ -42,7 +42,7 @@ const queries = ()=>{
     AND c.id IN (1308,1307,1306,1305,1304,1303,1302,1301,1300,1299,1298,1297,1296,1295,1294,1293,1292,1291,1290,1289,1288,1287,1286,1285)
     AND u.id IN (55,497,1547)
     ORDER BY ALUMNO, CURSOS DESC;`
-    //bloque 4 24-24
+    //bloque 4 24-25
     const posgrado = `SELECT u.username AS MATRICULA, CONCAT(u.firstname," ", u.lastname) AS ALUMNO, c.fullname AS CURSOS
     ,CASE
 	WHEN u.id =618 THEN "NO APLICA"
@@ -65,7 +65,7 @@ const queries = ()=>{
     LEFT JOIN mdl_user_lastaccess lt ON  lt.courseid = c.id AND lt.userid = u.id
     WHERE
     r.roleid = 5
-    AND c.id IN (1253,1254,1255,1257,1258,1259,1260,1261,1262,1263,1264,1265,1256)
+    AND c.id IN (1269,1270,1271,1267,1268,1272,1273,1274,1275,1276,1277,1278,1279)
     AND u.id NOT IN (618)
     AND (DATEDIFF("${TodaysDate}",FROM_UNIXTIME(timeaccess)) >=9 OR DATEDIFF("${TodaysDate}",FROM_UNIXTIME(timeaccess)) IS NULL)
     ORDER BY CURSOS ASC, ALUMNO;`
